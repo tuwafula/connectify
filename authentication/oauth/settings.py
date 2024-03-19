3c44bea579d3507cc8623b0132d36eae526a8f31
wafula <wafulaat@gmail.com>
2024-03-19 23:09:38 +0300

    add register and login features

--- authentication/oauth/settings.py
+++ authentication/oauth/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'rest_framework',
+
+    # my apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -100,12 +106,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTH_USER_MODEL = 'accounts.User'
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
